cee6400d3e1064fd6b1bac0478f2282897cd6d2c
cathey191 <cathey191@hotmail.com>
2018-05-31 11:31:42 +1200

    setup playing sounds

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 (function() {
 	var container = document.querySelector('.container');
+	var rainSound = new Audio('audio/rainFalling.wav');
+	var thunderSound = new Audio('audio/thunderstorm.wav');
 
 	container.addEventListener('click', sounds, false);
 
@@ -8,22 +10,62 @@
 		var buttonClass = buttonVar.className;
 		var nextSibling = buttonVar.nextElementSibling;
 		var previousSibling = buttonVar.previousElementSibling;
+		var section = buttonVar.parentElement;
 
 		if (buttonClass === 'strong') {
 			buttonVar.style.border = '2px solid #05c46b';
 			// reset siblings
 			nextSibling.style.border = '2px solid #b2bec3';
 			nextSibling.nextElementSibling.style.border = '2px solid #b2bec3';
+
+			// play sound
+			if (section.className === 'rain section') {
+				rainSound.play();
+				console.dir(rainSound);
+			} else if (section.className === 'hail section') {
+
+			} else if (section.className === 'wind section') {
+
+			} else if (section.className === 'thunder section') {
+
+			}
+
+
 		} else if (buttonClass === 'middle') {
 			buttonVar.style.border = '2px solid #05c46b';
 			// reset siblings
 			previousSibling.style.border = '2px solid #b2bec3';
 			nextSibling.style.border = '2px solid #b2bec3';
+
+			// play sound
+			if (section.className === 'rain section') {
+				// rainSound.play();
+			} else if (section.className === 'hail section') {
+
+			} else if (section.className === 'wind section') {
+
+			} else if (section.className === 'thunder section') {
+
+			}
+
+
 		} else if (buttonClass === 'low') {
 			buttonVar.style.border = '2px solid #05c46b';
 			// reset siblings
 			previousSibling.style.border = '2px solid #b2bec3';
 			previousSibling.previousElementSibling.style.border = '2px solid #b2bec3';
+
+			// play sound
+			if (section.className === 'rain section') {
+
+			} else if (section.className === 'hail section') {
+
+			} else if (section.className === 'wind section') {
+
+			} else if (section.className === 'thunder section') {
+				thunderSound.play();
+			}
+
 		}
 	} // sounds function close
 
