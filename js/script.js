29d5fab6762bc053e3665981d9899eb925edb6d5
Katie elizabeth French <845855192@bun2-206-16.local>
2018-05-31 12:18:01 +1200

    linked in audio files to js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 (function() {
 	var container = document.querySelector('.container');
-	var rainStrong = new Audio('audio/rainFalling.wav');
-	var thunderSound = new Audio('audio/thunderstorm.wav');
+	var rainLow = new Audio('audio/lightRain.mp3');
+	var rainMiddle = new Audio('audio/mediumRain.mp3');
+	var rainStrong = new Audio('audio/heavyRain.mp3');
+	var thunderLow = new Audio('audio/lightThunder.wav');
+	var thunderMiddle = new Audio('audio/mediumThunder.wav');
+	var thunderStrong = new Audio('audio/heavyThunder.wav');
+	var hailLow = new Audio('audio/lightHail.mp3');
+	var hailMiddle = new Audio('audio/mediumHail.mp3');
+	var hailStrong = new Audio('audio/heavyHail.wav');
+	var windLow = new Audio('audio/lightWind.wav');
+	var windMiddle = new Audio('audio/mediumWind.wav');
+	var windStrong = new Audio('audio/heavyWind.wav');
 
 	container.addEventListener('click', sounds, false);
 
@@ -13,63 +23,91 @@
 		var section = buttonVar.parentElement;
 
 		if (buttonClass === 'strong') {
-			buttonVar.style.border = '2px solid #05c46b';
-			// reset siblings
-			nextSibling.style.border = '2px solid #b2bec3';
-			nextSibling.nextElementSibling.style.border = '2px solid #b2bec3';
+					buttonVar.style.border = '2px solid #05c46b';
+					// reset siblings
+					nextSibling.style.border = '2px solid #b2bec3';
+					nextSibling.nextElementSibling.style.border = '2px solid #b2bec3';
 
-			// play sound
-			if (section.className === 'rain section') {
-				rainStrong.play();
-				// rainMiddle.pause();
-				// rainLow.pause();
-			} else if (section.className === 'hail section') {
+					// play sound
+					if (section.className === 'rain section') {
+									rainStrong.play();
+									rainMiddle.pause();
+									rainLow.pause();
+					} else if (section.className === 'hail section') {
+									hailStrong.play();
+									hailMiddle.pause();
+									hailLow.pause();
+					} else if (section.className === 'wind section') {
+									windStrong.play();
+									windMiddle.pause();
+									windLow.pause();
+					} else if (section.className === 'thunder section') {
+									thunderStrong.play();
+									thunderMiddle.pause();
+									thunderLow.pause();
+					}
 
-			} else if (section.className === 'wind section') {
-
-			} else if (section.className === 'thunder section') {
-
-			}
-
+					//-----------------------------------------------------------------
+					// END OF STRONG
+					//-----------------------------------------------------------------
 
 		} else if (buttonClass === 'middle') {
-			buttonVar.style.border = '2px solid #05c46b';
-			// reset siblings
-			previousSibling.style.border = '2px solid #b2bec3';
-			nextSibling.style.border = '2px solid #b2bec3';
+					buttonVar.style.border = '2px solid #05c46b';
+					// reset siblings
+					previousSibling.style.border = '2px solid #b2bec3';
+					nextSibling.style.border = '2px solid #b2bec3';
 
-			// play sound
-			if (section.className === 'rain section') {
-				rainStrong.pause();
-				// rainMiddle.play();
-				// rainLow.pause();
-			} else if (section.className === 'hail section') {
-
-			} else if (section.className === 'wind section') {
-
-			} else if (section.className === 'thunder section') {
-
-			}
-
-
+					// play sound
+					if (section.className === 'rain section') {
+								rainMiddle.play();
+								rainStrong.pause();
+								rainLow.pause();
+					} else if (section.className === 'hail section') {
+								hailMiddle.play();
+								hailStrong.pause();
+								hailLow.pause();
+					} else if (section.className === 'wind section') {
+								windMiddle.play();
+								windStrong.pause();
+								windLow.pause();
+					} else if (section.className === 'thunder section') {
+								thunderMiddle.play();
+								thunderStrong.pause();
+								thunderLow.pause();
+					}
+					//-----------------------------------------------------------------
+					// END OF MIDDLE
+					//-----------------------------------------------------------------
 		} else if (buttonClass === 'low') {
-			buttonVar.style.border = '2px solid #05c46b';
-			// reset siblings
-			previousSibling.style.border = '2px solid #b2bec3';
-			previousSibling.previousElementSibling.style.border = '2px solid #b2bec3';
+					buttonVar.style.border = '2px solid #05c46b';
+					// reset siblings
+					previousSibling.style.border = '2px solid #b2bec3';
+					previousSibling.previousElementSibling.style.border = '2px solid #b2bec3';
 
-			// play sound
-			if (section.className === 'rain section') {
-				rainStrong.pause();
-			} else if (section.className === 'hail section') {
+					// play sound
+					if (section.className === 'rain section') {
+								rainLow.play();
+								rainMiddle.pause();
+								rainStrong.pause();
+					} else if (section.className === 'hail section') {
+								hailLow.play();
+								hailMiddle.pause();
+								hailStrong.pause();
+					} else if (section.className === 'wind section') {
+								windLow.play();
+								windMiddle.pause();
+								windStrong.pause();
+					} else if (section.className === 'thunder section') {
+								thunderLow.play();
+								thunderMiddle.pause();
+								thunderStrong.pause();
+					}
 
-			} else if (section.className === 'wind section') {
+				}
+				//-----------------------------------------------------------------
+				// END OF LOW
+				//-----------------------------------------------------------------
 
-			} else if (section.className === 'thunder section') {
-				thunderSound.play();
-			}
-
-		}
 	} // sounds function close
 
 	// iffee close
