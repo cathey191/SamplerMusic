8fef5d1f259ff46bbbfc857e4ab4f43b98522aae
cathey191 <cathey191@hotmail.com>
2018-06-01 10:38:13 +1200

    Improved JS

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 (function() {
 	var container = document.querySelector('.container');
-	var rainLow = new Audio('audio/lightRain.mp3');
-	var rainMiddle = new Audio('audio/mediumRain.mp3');
-	var rainStrong = new Audio('audio/heavyRain.mp3');
-	var thunderLow = new Audio('audio/lightThunder.wav');
-	var thunderMiddle = new Audio('audio/mediumThunder.wav');
-	var thunderStrong = new Audio('audio/heavyThunder.wav');
-	var hailLow = new Audio('audio/lightHail.mp3');
-	var hailMiddle = new Audio('audio/mediumHail.mp3');
-	var hailStrong = new Audio('audio/heavyHail.wav');
-	var windLow = new Audio('audio/lightWind.wav');
-	var windMiddle = new Audio('audio/mediumWind.wav');
-	var windStrong = new Audio('audio/heavyWind.wav');
+	var noise = {
+	 	rainLow : new Audio('audio/lightRain.mp3'),
+		rainMiddle : new Audio('audio/mediumRain.mp3'),
+	 	rainStrong : new Audio('audio/heavyRain.mp3'),
+	 	thunderLow : new Audio('audio/lightThunder.wav'),
+		thunderMiddle : new Audio('audio/mediumThunder.wav'),
+		thunderStrong : new Audio('audio/heavyThunder.wav'),
+		hailLow : new Audio('audio/lightHail.mp3'),
+		hailMiddle : new Audio('audio/mediumHail.mp3'),
+		hailStrong : new Audio('audio/heavyHail.wav'),
+		windLow : new Audio('audio/lightWind.wav'),
+		windMiddle : new Audio('audio/mediumWind.wav'),
+		windStrong : new Audio('audio/heavyWind.wav')
+	}
 
 	container.addEventListener('click', sounds, false);
 
@@ -21,91 +23,39 @@
 		var nextSibling = buttonVar.nextElementSibling;
 		var previousSibling = buttonVar.previousElementSibling;
 		var section = buttonVar.parentElement;
+		var weather = section.classList[0];
+		var current = weather+buttonClass;
+		var strong = weather+"Strong";
+		var middle = weather+"Middle";
+		var low = weather+"Low"
+		console.dir(typeof e.target.tagName);
 
-		if (buttonClass === 'strong') {
+		noise[strong].pause();
+		noise[middle].pause();
+		noise[low].pause();
+
+		noise[current].play();
+
+		if (buttonClass === 'Strong') {
 			buttonVar.style.border = '2px solid #05c46b';
 			// reset siblings
 			nextSibling.style.border = '2px solid #b2bec3';
 			nextSibling.nextElementSibling.style.border = '2px solid #b2bec3';
 
-			// play sound
-			if (section.className === 'rain section') {
-				rainStrong.play();
-				rainMiddle.pause();
-				rainLow.pause();
-			} else if (section.className === 'hail section') {
-				hailStrong.play();
-				hailMiddle.pause();
-				hailLow.pause();
-			} else if (section.className === 'wind section') {
-				windStrong.play();
-				windMiddle.pause();
-				windLow.pause();
-			} else if (section.className === 'thunder section') {
-				thunderStrong.play();
-				thunderMiddle.pause();
-				thunderLow.pause();
-			}
-
-			//-----------------------------------------------------------------
-			// END OF STRONG
-			//-----------------------------------------------------------------
-		} else if (buttonClass === 'middle') {
+		} else if (buttonClass === 'Middle') {
 			buttonVar.style.border = '2px solid #05c46b';
 			// reset siblings
 			previousSibling.style.border = '2px solid #b2bec3';
 			nextSibling.style.border = '2px solid #b2bec3';
 
-			// play sound
-			if (section.className === 'rain section') {
-				rainMiddle.play();
-				rainStrong.pause();
-				rainLow.pause();
-			} else if (section.className === 'hail section') {
-				hailMiddle.play();
-				hailStrong.pause();
-				hailLow.pause();
-			} else if (section.className === 'wind section') {
-				windMiddle.play();
-				windStrong.pause();
-				windLow.pause();
-			} else if (section.className === 'thunder section') {
-				thunderMiddle.play();
-				thunderStrong.pause();
-				thunderLow.pause();
-			}
-			//-----------------------------------------------------------------
-			// END OF MIDDLE
-			//-----------------------------------------------------------------
-		} else if (buttonClass === 'low') {
+		} else if (buttonClass === 'Low') {
 			buttonVar.style.border = '2px solid #05c46b';
 			// reset siblings
 			previousSibling.style.border = '2px solid #b2bec3';
 			previousSibling.previousElementSibling.style.border = '2px solid #b2bec3';
 
-			// play sound
-			if (section.className === 'rain section') {
-				rainLow.play();
-				rainMiddle.pause();
-				rainStrong.pause();
-			} else if (section.className === 'hail section') {
-				hailLow.play();
-				hailMiddle.pause();
-				hailStrong.pause();
-			} else if (section.className === 'wind section') {
-				windLow.play();
-				windMiddle.pause();
-				windStrong.pause();
-			} else if (section.className === 'thunder section') {
-				thunderLow.play();
-				thunderMiddle.pause();
-				thunderStrong.pause();
-			}
 		}
-		//-----------------------------------------------------------------
-		// END OF LOW
-		//-----------------------------------------------------------------
-	} // sounds function close
 
-	// iffee close
+	}
+
 })();
